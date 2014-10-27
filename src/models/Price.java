4d28c1ad54e7d325c6ba9d05eecc30c4fe0e955c
Chris Diebold <cdiebold2012@my.fit.edu>
2014-10-27 18:27:36 -0400

    added Price.java

--- src/models/Price.java
+++ src/models/Price.java
@@ -1,5 +1,38 @@
 package models;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
 public class Price {
+	private static int ROUNDING_MODE = BigDecimal.ROUND_HALF_EVEN;
+	private static int DECIMALS = Currency.getInstance("USD").getDefaultFractionDigits(); //In the US this is 2.
+	private BigDecimal price;
+	
+	public Price(String money)
+	{
+		this.price  = new BigDecimal(money);
+	}
+	
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public BigDecimal subtractFromBalance(String balanceInAccount)
+	{
+		BigDecimal balance = new BigDecimal(balanceInAccount);
+		
+		BigDecimal result = balance.subtract(this.getPrice());
+		
+		return result;
+	}
+	public BigDecimal subtractFromBalance(BigDecimal other)
+	{
+		return other.subtract(this.getPrice());
+	}
+	
 
 }
